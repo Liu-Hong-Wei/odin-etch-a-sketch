533e4995929efa197d63546e30895ebec6cad015
homeylau <lhw1953927853@gmail.com>
2024-10-15 11:33:36 +0800

    add color animation while mouse moving

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ const gridContainer = document.createElement("div");
 gridContainer.id = "grid-container";
 body.appendChild(gridContainer);
 
-for (let i = 0; i < 16 * 16; i++) {
+let width = 16;
+let height = 16;
+for (let i = 0; i < width * height; i++) {
   const gridDiv = document.createElement("div");
   gridDiv.className = "grid-box";
+  gridDiv.addEventListener("mouseleave", (e) => {
+    gridDiv.style.backgroundColor = "lightblue";
+  });
   gridContainer.appendChild(gridDiv);
 }
